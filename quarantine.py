b36bf806479184586df677ebbae3695aa41476f7
SachinNegi01 <148526818+SachinNegi01@users.noreply.github.com>
2025-04-19 18:19:49 +0530

    Delete quarantine.py

--- quarantine.py
+++
@@ -1,9 +0,0 @@
-import os
-import signal
-
-def kill_process(pid):
-    try:
-        os.kill(pid, signal.SIGTERM)
-        print(f"Terminated PID: {pid}")
-    except ProcessLookupError:
-        print("Process not found!")
